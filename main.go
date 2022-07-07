e01033aab6e0c9178e901855c70875daa6959500
huder522 <huder.delacruz.522@gmail.com>
2022-07-07 12:37:34 -0500

    division function added

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-// functions
+/*
+	FUNCTIONS
+*/
+
+// add function
 func add() {
 
 	var num1, num2 int
@@ -18,10 +22,31 @@ func add() {
 
 }
 
+// division function
+func division() {
+
+	var num1, num2 float32
+
+	fmt.Println( "This program will be to divides two numbers and returns the quotient and remainder:" )
+
+	fmt.Println( "Input the number 1" )
+	fmt.Scanln( &num1)
+	fmt.Println( "Input the number 2" )
+	fmt.Scanln( &num2 )
+
+	var quotient float32 = num1 / num2
+	var remainder  = int(num1) % int(num2)
+
+	fmt.Println( "The quotient of these devision is: ", quotient )
+	fmt.Println( "The remainder of these devision is: ", remainder )
+
+}
 
 
 func main() {
 	
 	add()
+
+	division()
 
 }
