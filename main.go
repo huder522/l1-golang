ef5c14795d57f570f4d7b79201c1cc376f4a5c0e
huder522 <huder.delacruz.522@gmail.com>
2022-07-07 12:24:35 -0500

    function add added

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import "fmt"
 
+// functions
+func add() {
+
+	var num1, num2 int
+
+	fmt.Println( "This program will be to add two numbers:" )
+
+	fmt.Println( "Input the number 1" )
+	fmt.Scanln( &num1)
+	fmt.Println( "Input the number 2" )
+	fmt.Scanln( &num2 )
+
+	fmt.Println( "The sum of the numbers is: ", num1 + num2 )
+
+}
+
+
+
 func main() {
 	
-	fmt.Println("Hello world")
-	
+	add()
+
 }
